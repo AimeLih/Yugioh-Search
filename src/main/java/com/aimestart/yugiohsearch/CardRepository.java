54684cd057a20a6f9aea87af3cc1fc28d58a2e7d
AimeLih <aimelihono@gmail.com>
2026-05-21 15:52:38 -0500

    Added method to fetch a card by its name in CardRepository

--- src/main/java/com/aimestart/yugiohsearch/CardRepository.java
+++ src/main/java/com/aimestart/yugiohsearch/CardRepository.java
@@ -2,5 +2,5 @@ package com.aimestart.yugiohsearch;
 import org.springframework.data.jpa.repository.JpaRepository;
 public interface CardRepository extends JpaRepository<Card, Long>{
     boolean existsByName(String name);
-
+    Card getCardByName(String name);
 }
