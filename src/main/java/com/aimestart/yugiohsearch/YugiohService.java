3b727b58d3d87a3ced6e6a2671da584145ae12e2
AimeLih <aimelihono@gmail.com>
2026-05-31 17:27:18 -0500

    Added a new function that updates my current card repository type and weight column

--- src/main/java/com/aimestart/yugiohsearch/YugiohService.java
+++ src/main/java/com/aimestart/yugiohsearch/YugiohService.java
@@ -5,6 +5,7 @@ import org.springframework.web.client.RestClient;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 @Service
 public class YugiohService {
@@ -14,7 +15,7 @@ public class YugiohService {
 
     public record CardInfoResponse(List<CardData> data) {}
 
-    public record CardData(String name, String desc) {}
+    public record CardData(String name, String desc, String type) {}
 
     public YugiohService(RestClient.Builder builder, CardRepository cardRepository) {
         this.restClient = builder.baseUrl("https://db.ygoprodeck.com/api/v7").build();
@@ -38,15 +39,58 @@ public class YugiohService {
     public List<Card> importAllCards() {
         List<CardData> apiCards = fetchallCards();
         List<Card> cardsToSave = new ArrayList<>();
-
         for (CardData card : apiCards) {
             if (!cardRepository.existsByName(card.name())) {
-                cardsToSave.add(new Card(card.name(), card.desc()));
+                cardsToSave.add(new Card(card.name(), card.desc(), card.type(), 0));
             }
         }
         return cardRepository.saveAll(cardsToSave);
     }
 
+    public void updateExistingCards() {
+        List<Card> cards = cardRepository.findAll();
+        List<CardData> apiCards = fetchallCards();
+        for (Card card : cards) {
+            for (CardData apiCard : apiCards) {
+                if (card.getName().equals(apiCard.name())) {
+                    String TYPE = apiCard.type();
+                    card.setType(TYPE);
+                    boolean switchwork = false;
+                    switch(TYPE){
+                        case "Spell Card":
+                            card.setWeight(4);
+                            switchwork = true;
+                            break;
+                        case "Trap Card":
+                            card.setWeight(3);
+                            switchwork = true;
+                            break;
+                        case "Effect Monster":
+                            card.setWeight(2);
+                            switchwork = true;
+                            break;
+                        case "Normal Monster":
+                            card.setWeight(1);
+                            switchwork = true;
+                            break;
+                        default:
+                    }
+                    if(!switchwork){
+                        if(TYPE.contains("Spell")){
+                            card.setWeight(4);
+                        } else if (TYPE.contains("Trap")) {
+                            card.setWeight(3);
+                        } else if (TYPE.contains("Monster")) {
+                            card.setWeight(2);
+                        } else {
+                            card.setWeight(1);
+                        }
+                    }
+                    cardRepository.save(card);
+                }
+            }
+        }
+    }
     public Card getCardByName(String name) {
         return cardRepository.getCardByName(name);
     }
@@ -63,5 +107,12 @@ public class YugiohService {
             throw new RuntimeException("No cards found with that name");
         }
         return cards;
+    }
+
+    public void AssigningWeight(){
+        for (Card card : cardRepository.findAll()) {
+            card.setWeight(1);
+            cardRepository.save(card);
+        }
     }
 }
