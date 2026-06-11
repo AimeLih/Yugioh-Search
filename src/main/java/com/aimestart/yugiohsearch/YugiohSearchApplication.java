5dbc35ef95a2eb72e46ed8fa57272530d9289dab
AimeLih <aimelihono@gmail.com>
2026-06-10 19:37:05 -0500

    Enabled caching in YugiohSearchApplication with `@EnableCaching`.

--- src/main/java/com/aimestart/yugiohsearch/YugiohSearchApplication.java
+++ src/main/java/com/aimestart/yugiohsearch/YugiohSearchApplication.java
@@ -2,8 +2,10 @@ package com.aimestart.yugiohsearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class YugiohSearchApplication {
 
     public static void main(String[] args) {
